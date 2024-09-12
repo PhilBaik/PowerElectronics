b1b4a0dc0af816da8dc3a87190ecc65ea9638051
Phil_Baik_Mac <hmbaik97@gmail.com>
2024-09-12 12:55:43 -0500

    Feat: Data gen automation

---
+++ Data_generator/main_data.m
@@ -0,0 +1,6 @@
+clc;
+clear;
+
+for ite = 1:1:20
+    run('main_data_generator.m')
+end
--- Data_generator/main_data_generator.m
+++ Data_generator/main_data_generator.m
@@ -1,17 +1,15 @@
 restoredefaultpath
 delete(gcp('nocreate'));
 clc;
-clear;
+clear all;
 close all;
-addpath('Parallel_Computing/')
 
 %% Case 1
 nummodel = 1;
-numcontrol = 40;
-numinputsim = 14;
+numcontrol = 32;
+numinputsim = 16;
 
 numSims = nummodel*numcontrol*numinputsim;
-
 
 mdl = 'buck_converter_data_gen';
 isModelOpen = bdIsLoaded(mdl);
@@ -22,6 +20,8 @@ Fs_min = 1e3;
 Fs_max = 1e6;
 Rdson_min = 1e-4;
 Rdson_max = 1e-2;
+Rdon_min = 1e-4;
+Rdon_max = 1e-2;
 Kp_min = 0;
 Kp_max = 1;
 Vref_min = 10;
@@ -34,14 +34,18 @@ Co_min = 0;
 Co_max = 1e-8;
 Vg_min = 10;
 Vg_max = 3300;
+Vf_min = 0;
+Vf_max = 2;
+
 %% Test
 i=1;
 Fs(1,i) = rand_gen(Fs_min,Fs_max);
-Fsamp(1,i) = Fs(1,i)*500;
+Fsamp(1,i) = Fs(1,i)*100;
 Ts(1,i) = 1/Fs(1,i);
 Tsamp(1,i) = 1/Fsamp(1,i);
 
 Rdson(1,i) = rand_gen(Rdson_min,Rdson_max);
+Rdon(1,i) = rand_gen(Rdon_min,Rdon_max);
 
 Kp(1,i) = rand_gen(Kp_min,Kp_max);
 Ki(1,i) = rand_gen(Kp_min,Kp_max);
@@ -53,7 +57,7 @@ Ttrig1(1,i) = Tend(1,i)/3;
 Ttrig2(1,i) = Tend(1,i)*2/3;
 
 Vg(1,i) = rand_gen(Vg_min,Vg_max);
-
+Vf(1,i) = rand_gen(Vf_min,Vf_max);
 
 Vref1(1,i) = rand_gen(Vref_min,Vref_max);
 Vref2(1,i) = rand_gen(Vref_min,Vref_max);
@@ -73,9 +77,10 @@ for l = 1:1:nummodel
     Ttrig1(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = Tend(1,(l-1)*numinputsim*numcontrol+1)/3;
     Ttrig2(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = Tend(1,(l-1)*numinputsim*numcontrol+1)*2/3;
 
-    
+    Rdon(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = rand_gen(Rdon_min,Rdon_max);
     Rdson(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = rand_gen(Rdson_min,Rdson_max);
     Vg(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = rand_gen(Vg_min,Vg_max);
+    Vf(1,(l-1)*numinputsim*numcontrol+1:l*numinputsim*numcontrol) = rand_gen(Vf_min,Vf_max);
     for m = 1:1:numcontrol
         Kp(1,(l-1)*numinputsim*numcontrol+(m-1)*numinputsim+1:(l-1)*numinputsim*numcontrol+m*numinputsim) = rand_gen(Kp_min,Kp_max);
         Ki(1,(l-1)*numinputsim*numcontrol+(m-1)*numinputsim+1:(l-1)*numinputsim*numcontrol+m*numinputsim) = rand_gen(Kp_min,Kp_max);
@@ -98,7 +103,10 @@ end
 for i = 1:1:numSims
     in_temp(i) = Simulink.SimulationInput(mdl);
     in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'R', num2str(Ro(1,i)));
+    in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'Rdon', num2str(Rdon(1,i)));
+    in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'Rdson', num2str(Rdson(1,i)));
     in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'Vg', num2str(Vg(1,i)));
+    in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'Vf', num2str(Vf(1,i)));
     in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'C', num2str(Co(1,i)));
     in_temp(i) = setBlockParameter(in_temp(i),[mdl '/Buck_converter'], 'L', num2str(L(1,i)));
     in_temp(i) = setVariable(in_temp(i),'Vref1', Vref1(1,i));
@@ -114,36 +122,49 @@ for i = 1:1:numSims
     in(i) = in_temp(i);
 end
 %%
-out_sim = parsim(in, 'ShowProgress','on','TransferBaseWorkspaceVariables','on');
+out_sim = parsim(in, 'ShowProgress','on','TransferBaseWorkspaceVariables','off');
 delete(gcp('nocreate'));
+if(~isModelOpen)
+    close_system(mdl, 0);
+end
 
-
+%%
+simData = cell(1,numSims);
 %%
 for i = 1:1:numSims
     simData{i}.Variables = in(i).Variables;
     simData{i}.BlockParameters = in(i).BlockParameters;
-    simOut = out_sim(i);
-    simData{i}.time = simOut.logsout.get('vo').Values.Time';
-    simData{i}.vo = simOut.logsout.get('vo').Values.Data(:,1)';
-    simData{i}.vref = simOut.logsout.get('vref').Values.Data(:,1)';
-    simData{i}.gate = simOut.logsout.get('Gate').Values.Data(:,1)';
-    simData{i}.io = simOut.logsout.get('io').Values.Data(:,1)';
+    simData{i}.time = out_sim(i).logsout.get('vo').Values.Time';
+    simData{i}.vo = out_sim(i).logsout.get('vo').Values.Data(:,1)';
+    simData{i}.vref = out_sim(i).logsout.get('vref').Values.Data(:,1)';
+    simData{i}.gate = out_sim(i).logsout.get('Gate').Values.Data(:,1)';
+    simData{i}.io = out_sim(i).logsout.get('io').Values.Data(:,1)';
 end
 %%
 i=1;
-filename = sprintf('%s_%d.mat',date(),i);
-while exist(filename)
+foldername = sprintf('%s',date());
+mkdir(foldername);
+filename1 = sprintf('%s/ver%d_1_1.mat',date(),i);
+while exist(filename1)
     i = i+1;
-    filename = sprintf('%s_%d.mat',date(),i);
+    filename1 = sprintf('%s/ver%d_1_1.mat',date(),i);
 end
 
-save(filename,'simData')
+for l = 1:1:nummodel
+    for m = 1:1:numcontrol
+        filename_temp = sprintf('%s/ver%d_%d_%d.mat',date(),i,l,m);
+        temp_Data = simData(1,(l-1)*numinputsim*numcontrol+(m-1)*numinputsim+1:(l-1)*numinputsim*numcontrol+m*numinputsim);
+        save(filename_temp,'temp_Data')
+    end
+end
+
+
 %%
 close all
 
 figure(1)
 i=1;
-simOut = simData{i};
+simOut = temp_Data{1,1};
 % 'ts' is a MATLAB 'timeseries' object that stores the time and
 % data values for the logged 'vertical_disp' signal.
 % Use the 'plot' method of the object to plot the data against the
@@ -154,14 +175,15 @@ plot(simOut.time,simOut.vref,'DisplayName','vref, i=1');
 title('i = 1')
 
 figure(2)
-i=100;
-simOut = simData{numSims};
+simOut = temp_Data{1,end};
+% 'ts' is a MATLAB 'timeseries' object that stores the time and
+% data values for the logged 'vertical_disp' signal.
+% Use the 'plot' method of the object to plot the data against the
+% time.
 plot(simOut.time,simOut.vo,'DisplayName','vo, i=1');
 hold on
 plot(simOut.time,simOut.vref,'DisplayName','vref, i=1');
-title_name = sprintf('i = %d',i);
-title(title_name)
-% legend(legend_labels)
+title('i = end')
 %% local func
 function y= rand_gen(min_x, max_x)
     y=rand(1,1).*(max_x-min_x)+min_x;
